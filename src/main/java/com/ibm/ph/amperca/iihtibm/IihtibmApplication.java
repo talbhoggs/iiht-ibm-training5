6939ffb3d1438c352cbb5c57769b63fd9c382a28
Charles AMPER <amperca@ph.ibm.com>
2019-09-27 01:44:45 +0800

    initial commit

---
+++ src/main/java/com/ibm/ph/amperca/iihtibm/IihtibmApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.ph.amperca.iihtibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IihtibmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IihtibmApplication.class, args);
+	}
+
+}
