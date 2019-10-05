a5386c0582052ad68d79513ad4984c88dba349d4
Charles AMPER <amperca@ph.ibm.com>
2019-10-06 07:34:20 +0800

    Add role when creating a user

--- src/main/java/com/ibm/ph/amperca/iihtibm/repository/RoleRepository.java
+++ src/main/java/com/ibm/ph/amperca/iihtibm/repository/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.ibm.ph.amperca.iihtibm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import com.ibm.ph.amperca.iihtibm.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+  @Query("SELECT t FROM Role t where t.name = :name")
+  Role findByRoleName(String name);
 
 }
