64607e1c34ea94dceaec41b1806f7cfbda24dd9a
Charles AMPER <amperca@ph.ibm.com>
2019-10-06 07:19:06 +0800

    Implement update functionality

--- src/main/java/com/ibm/ph/amperca/iihtibm/config/WebSecurityConfig.java
+++ src/main/java/com/ibm/ph/amperca/iihtibm/config/WebSecurityConfig.java
@@ -17,51 +17,49 @@ import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+  @Autowired
+  private UserDetailsService userDetailsService;
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().
-                antMatchers("/login").permitAll().
-                antMatchers("/about").permitAll().
-                antMatchers("/logout").permitAll().
-                antMatchers("/signup").permitAll().
-                antMatchers("/login/**").permitAll().
-                antMatchers("/signup/**").permitAll().
-                antMatchers("/h2-console/**").permitAll().
-                antMatchers("/main").hasAnyAuthority("USER","ADMIN").
-                antMatchers("/admin/**").hasAnyAuthority("ADMIN").anyRequest().authenticated().
-                and().formLogin()
-                .loginPage("/login").defaultSuccessUrl("/main", true).failureUrl("/login?error=true").and()
-                .exceptionHandling().accessDeniedPage("/403").and().logout().logoutSuccessUrl("/login?logout=true")
-                .invalidateHttpSession(true).permitAll().and().csrf().disable();
-        http.headers().frameOptions().disable();
-    }
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/about").permitAll()
+        .antMatchers("/logout").permitAll().antMatchers("/signup").permitAll()
+        .antMatchers("/login/**").permitAll().antMatchers("/signup/**").permitAll()
+        .antMatchers("/h2-console/**").permitAll().antMatchers("/main")
+        .hasAnyAuthority("USER", "ADMIN").antMatchers("/accountupdate")
+        .hasAnyAuthority("USER", "ADMIN").antMatchers("/accountupdate**")
+        .hasAnyAuthority("USER", "ADMIN").antMatchers("/admin/**").hasAnyAuthority("ADMIN")
+        .anyRequest().authenticated().and().formLogin().loginPage("/login")
+        .defaultSuccessUrl("/main", true).failureUrl("/login?error=true").and().exceptionHandling()
+        .accessDeniedPage("/403").and().logout().logoutSuccessUrl("/login?logout=true")
+        .invalidateHttpSession(true).permitAll().and().csrf().disable();
+    http.headers().frameOptions().disable();
+  }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**/*.html", "/**/*.css", "/**/*.js", "/**/*.{png,jpg,jpeg,svg.ico}");
-    }
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/**/*.html", "/**/*.css", "/**/*.js",
+        "/**/*.{png,jpg,jpeg,svg.ico}");
+  }
 
-    @Bean
-    public AuthenticationManager customAuthenticationManager() throws Exception {
-        return authenticationManager();
-    }
+  @Bean
+  public AuthenticationManager customAuthenticationManager() throws Exception {
+    return authenticationManager();
+  }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+  }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
-    @Bean
-    public SpringSecurityDialect springSecurityDialect(){
-        return new SpringSecurityDialect();
-    }
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SpringSecurityDialect springSecurityDialect() {
+    return new SpringSecurityDialect();
+  }
 
 }
